82559f039e59d7d17917df0041fb7ee257b0c8d9
david_cool721@yahoo.com <david_cool721@yahoo.com>
2018-05-23 22:52:42 +0900

    commented

--- Blockchain.cs
+++ Blockchain.cs
@@ -16,6 +16,11 @@ class Blockchain{
    public void addUnhandleBlock(int id,int size,double fee){
         Open_transaction.Add(new Transaction(id,size,fee));
    }
+   /*
+    This is the core mining function to mine the selected transaction in a Block.
+    This is just a very rough design. This function should consits of the transaction validation, hashing function, 
+    and the mininig reward to the miner's digital wallet(Public key).
+    */
     public Tuple<int,double> mineBlock(){
          int blockSize =0;
           double totalFee=0.0;
